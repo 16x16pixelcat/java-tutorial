e0befcef988c25a0c05c5d503bb58e1fce8d5a69
16x16pixelcat <16x16pixelcat@gmail.com>
2026-04-22 15:58:06 +0300

    innit commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(){
+
+        /*
+            Prints
+        a way to display text in terminal
+        useful for debugging I think
+         */
+
+        System.out.print("cats are cool\n");    // the \n adds a line after the print
+        System.out.println("Me Loff Catzz");    // println adds a line after prints
+        System.out.print("me need catzz");  // Prints what's in the quotes
+        System.out.println();   //sout quickly add a Print line
+    }
+}
