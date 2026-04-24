f36657115cf0eac785dfa85d76090783d777f553
16x16pixelcat <16x16pixelcat@gmail.com>
2026-04-24 23:46:02 +0300

    Strings

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,34 @@ public class Main {
 
 
         //---------------integers----------------------
-        // int age = 18;
-        // int year = 2026;
-        // int quantity = 5;
-        //
-        // System.out.println(quantity);
-        //
-        // System.out.println("the year is " + year);
-        //
-        // System.out.println(age);
+        int age = 18;
+        int year = 2026;
+        int quantity = 5;
+
+        System.out.println(quantity);
+
+        System.out.println("the year is " + year);
+
+        System.out.println(age);
         //---------------double------------------------
-        //double price = 29.99;
-        //double temperature = -12;
-        //
-        //System.out.println(temperature);
-        //
-        //System.out.println("JOD " + price);
+        double price = 29.99;
+        double temperature = -12;
+
+        System.out.println(temperature);
+
+        System.out.println("JOD " + price);
         //---------------char--------------------------
-        //char grade = 'f';
-        //
-        //char symbol = '!';
-        //
-        //char currency = '$';
-        //
-        //System.out.println(currency);
-        //
-        //System.out.println(symbol);
-        //
-        //System.out.println(grade);
+        char grade = 'f';
+
+        char symbol = '!';
+
+        char currency = '$';
+
+        System.out.println(currency);
+
+        System.out.println(symbol);
+
+        System.out.println(grade);
         //----------------boolean----------------------
         boolean cat = true;
 
@@ -39,15 +39,33 @@ public class Main {
 
         boolean catsAreBetter = true;
 
-        System.out.println(CatsAreBetter);
+        System.out.println(catsAreBetter);
 
-        System.out.println(LikeDogs);
+        System.out.println(likeDogs);
 
         System.out.println(cat);
+        //----------------string-----------------------
+        String name = "Pixel";
+
+        String food = "Mansaf";
+
+        String email = "cat123@catmail.com";
+
+        System.out.println(name + " " + food + " " + email);
+
+        System.out.println(email);
+
+        System.out.println("my favorite food is " + food);
+
+        System.out.println("Hi my name is " + name);
         //----------------print------------------------
-        //System.out.print("cats are cool\n");    // the \n adds a line after the print
-        //System.out.println("Me Loff Catzz");    // println adds a line after prints
-        //System.out.print("me need catzz");  // Prints what's in the quotes
-        //System.out.println();   //sout quickly add a Print line
+        //System.out.print("cats are cool\n");
+        // the \n adds a line after the print
+        //System.out.println("Me Loff Catzz");
+        // println adds a line after prints
+        //System.out.print("me need catzz");
+        // Prints what's in the quotes
+        //System.out.println();
+        // sout quickly add a Print line
     }
 }
