d5a0e4c8bde8d42337b81d9a0e34ef1abbdfa4c5
16x16pixelcat <16x16pixelcat@gmail.com>
2026-04-24 14:36:00 +0300

    Primitive Variable

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,52 @@ public class Main {
 
     public static void main(){
 
-        /*
-            Prints
-        a way to display text in terminal
-        useful for debugging I think
-         */
 
-        System.out.print("cats are cool\n");    // the \n adds a line after the print
-        System.out.println("Me Loff Catzz");    // println adds a line after prints
-        System.out.print("me need catzz");  // Prints what's in the quotes
-        System.out.println();   //sout quickly add a Print line
+        //---------------integers----------------------
+        // int age = 18;
+        // int year = 2026;
+        // int quantity = 5;
+        //
+        // System.out.println(quantity);
+        //
+        // System.out.println("the year is " + year);
+        //
+        // System.out.println(age);
+        //---------------double------------------------
+        //double price = 29.99;
+        //double temperature = -12;
+        //
+        //System.out.println(temperature);
+        //
+        //System.out.println("JOD " + price);
+        //---------------char--------------------------
+        //char grade = 'f';
+        //
+        //char symbol = '!';
+        //
+        //char currency = '$';
+        //
+        //System.out.println(currency);
+        //
+        //System.out.println(symbol);
+        //
+        //System.out.println(grade);
+        //----------------boolean----------------------
+        boolean cat = true;
+
+        boolean likeDogs = false;
+
+        boolean catsAreBetter = true;
+
+        System.out.println(CatsAreBetter);
+
+        System.out.println(LikeDogs);
+
+        System.out.println(cat);
+        //----------------print------------------------
+        //System.out.print("cats are cool\n");    // the \n adds a line after the print
+        //System.out.println("Me Loff Catzz");    // println adds a line after prints
+        //System.out.print("me need catzz");  // Prints what's in the quotes
+        //System.out.println();   //sout quickly add a Print line
     }
 }
